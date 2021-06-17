28862284103cf87cf2280b7bd6174cdaa26dac7f
HuemanTarget <joshbasche@live.com>
2021-06-17 09:17:43 -0500

    first commit

---
+++ WickedTest/WickedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WickedTestApp.swift
+//  WickedTest
+//
+//  Created by Joshua Basche on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WickedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
