8a0fab52b1ec6ee23baacea25260b3f0eb6a4e57
HuemanTarget <joshbasche@live.com>
2021-06-17 09:39:09 -0500

    setup tabbarview

--- WickedTest/ContentView.swift
+++ WickedTest/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    NavigationView {
+      TabView {
+        Text("Feed")
+          .tabItem {
+            Image(systemName: "house")
+            Text("Home")
+          }
+        
+        Text("Search")
+          .tabItem {
+            Image(systemName: "magnifyingglass")
+            Text("Search")
+          }
+        
+        Text("Message")
+          .tabItem {
+            Image(systemName: "envelope")
+            Text("Messages")
+          }
+      }
+      .navigationBarTitle("Home", displayMode: .inline)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
