58376f04469c668dc7305e3aed165e4f160ca6fc
HuemanTarget <joshbasche@live.com>
2021-06-17 09:50:39 -0500

    create FeedView and floating action button

---
+++ WickedTest/View/FeedView.swift
@@ -0,0 +1,38 @@
+//
+//  FeedView.swift
+//  WickedTest
+//
+//  Created by Joshua Basche on 6/17/21.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+  var body: some View {
+    ZStack(alignment: .bottomTrailing) {
+      ScrollView {
+        
+      }
+      
+      Button(action: {
+        print("Button Pressed")
+      }) {
+        Image("Tweet")
+          .resizable()
+          .renderingMode(.template)
+          .frame(width: 32, height: 32)
+          .padding()
+      }
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .clipShape(Circle())
+      .padding()
+    }
+  }
+}
+
+struct FeedView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeedView()
+  }
+}
