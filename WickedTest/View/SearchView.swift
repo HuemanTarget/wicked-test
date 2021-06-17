44e0611d963e257b15749a342c4b3960dced7806
HuemanTarget <joshbasche@live.com>
2021-06-17 11:19:24 -0500

    create UserCell

--- WickedTest/View/SearchView.swift
+++ WickedTest/View/SearchView.swift
@@ -15,11 +15,13 @@ struct SearchView: View {
       SearchBar(text: $searchText)
         .padding()
       
-      VStack {
-        ForEach(0..<19) { _ in
-          Text("Add Users Here...")
+      VStack(alignment: .leading) {
+        ForEach(0..<10) { _ in
+          HStack { Spacer() }
+          UserCell()
         }
       }
+      .padding(.leading)
     }
   }
 }
