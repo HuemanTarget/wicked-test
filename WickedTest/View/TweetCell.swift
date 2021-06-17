653fe8d309d06c615a248183aa135116855c10f9
HuemanTarget <joshbasche@live.com>
2021-06-17 10:22:30 -0500

    create TweetCell

---
+++ WickedTest/View/TweetCell.swift
@@ -0,0 +1,83 @@
+//
+//  TweetCell.swift
+//  WickedTest
+//
+//  Created by Joshua Basche on 6/17/21.
+//
+
+import SwiftUI
+
+struct TweetCell: View {
+  var body: some View {
+    VStack {
+      HStack(alignment: .top) {
+        Image("batman")
+          .resizable()
+          .scaledToFill()
+          .clipped()
+          .frame(width: 56, height: 56)
+          .cornerRadius(56 / 2)
+          .padding(.leading)
+        VStack(alignment: .leading, spacing: 4) {
+          HStack {
+            Text("Bruce Wayne")
+              .font(.system(size: 14, weight: .semibold))
+            
+            Text("@batman •")
+              .foregroundColor(.gray)
+            Text("2w")
+              .foregroundColor(.gray)
+          }
+          
+          Text("It's not who I am underneath, but what I do that defines me")
+        }
+      }
+      .padding(.bottom)
+      .padding(.trailing)
+      HStack {
+        Button(action: {
+          
+        }) {
+          Image(systemName: "bubble.left")
+            .font(.system(size: 16))
+            .frame(width: 32, height: 32)
+        }
+        
+        Spacer()
+        Button(action: {
+          
+        }) {
+          Image(systemName: "arrow.2.squarepath")
+            .font(.system(size: 16))
+            .frame(width: 32, height: 32)
+        }
+        Spacer()
+        Button(action: {
+          
+        }) {
+          Image(systemName: "heart")
+            .font(.system(size: 16))
+            .frame(width: 32, height: 32)
+        }
+        Spacer()
+        Button(action: {
+          
+        }) {
+          Image(systemName: "bookmark")
+            .font(.system(size: 16))
+            .frame(width: 32, height: 32)
+        }
+      }
+      .foregroundColor(.gray)
+      .padding(.horizontal)
+      
+      Divider()
+    }
+  }
+}
+
+struct TweetCell_Previews: PreviewProvider {
+  static var previews: some View {
+    TweetCell()
+  }
+}
