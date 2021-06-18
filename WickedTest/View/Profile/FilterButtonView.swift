351549efb912a8ef07bb634e926c560877b2353b
HuemanTarget <joshbasche@live.com>
2021-06-18 09:57:32 -0500

    create FilterButtonView and add to UserProfileView

--- WickedTest/View/Profile/FilterButtonView.swift
+++ WickedTest/View/Profile/FilterButtonView.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
-struct FilterButtonView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+enum TweetFilterOptions: Int, CaseIterable {
+  case tweets
+  case replies
+  case likes
+  
+  var title: String {
+    switch self {
+    case .tweets: return "Tweets"
+    case .replies: return "Tweets & Replies"
+    case .likes: return "Likes"
     }
+  }
+}
+
+struct FilterButtonView: View {
+  private let underlineWidth = UIScreen.main.bounds.width / CGFloat(TweetFilterOptions.allCases.count)
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      HStack {
+        ForEach(TweetFilterOptions.allCases, id: \.self) { option in
+          Text(option.title)
+            .frame(width: underlineWidth)
+            .foregroundColor(.blue)
+        }
+      }
+      
+      Rectangle()
+        .frame(width: underlineWidth, height: 3)
+        .foregroundColor(.blue)
+        .animation(.spring())
+    }
+  }
 }
 
 struct FilterButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-        FilterButtonView()
-    }
+  static var previews: some View {
+    FilterButtonView()
+  }
 }
