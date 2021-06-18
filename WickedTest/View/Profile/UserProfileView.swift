8699442bae591ba8a039d14a9bd42e69b01f40e0
HuemanTarget <joshbasche@live.com>
2021-06-18 10:18:56 -0500

    create FilterButtonView

--- WickedTest/View/Profile/UserProfileView.swift
+++ WickedTest/View/Profile/UserProfileView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct UserProfileView: View {
+  @State var selectedFilter: TweetFilterOptions = .tweets
+  
   var body: some View {
     ScrollView {
       VStack {
         ProfileHeaderView()
           .padding()
         
-        FilterButtonView()
+        FilterButtonView(selectedOption: $selectedFilter)
+          .padding()
+        
+        ForEach(0..<9) { tweet in
+          TweetCell()
+            .padding(40)
+        }
       }
       
       .navigationTitle("batman")
