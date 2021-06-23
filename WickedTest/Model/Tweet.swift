3632b27617ac544b382b22719913db1346949145
HuemanTarget <joshbasche@live.com>
2021-06-23 12:06:03 -0500

    create FeedViewModel

---
+++ WickedTest/Model/Tweet.swift
@@ -0,0 +1,30 @@
+//
+//  Tweet.swift
+//  WickedTest
+//
+//  Created by Joshua Basche on 6/23/21.
+//
+
+import Firebase
+
+struct Tweet: Identifiable {
+  let id: String
+  let username: String
+  let profileImageUrl: String
+  let fullname: String
+  let caption: String
+  let likes: Int
+  let uid: String
+  let timestamp: Timestamp
+  
+  init(dictionary: [String: Any]) {
+    self.id = dictionary["uid"] as? String ?? ""
+    self.username = dictionary["username"] as? String ?? ""
+    self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    self.fullname = dictionary["fullname"] as? String ?? ""
+    self.caption = dictionary["caption"] as? String ?? ""
+    self.likes = dictionary["likes"] as? Int ?? 0
+    self.uid = dictionary["uid"] as? String ?? ""
+    self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+  }
+}
